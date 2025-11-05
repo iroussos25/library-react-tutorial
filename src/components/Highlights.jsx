2d481d480b391119e80f220c9824a979c5d18ab5
Yiannis <ioannis.roussos@hotmail.com>
2025-11-05 15:10:21 +0200

    2.5 done

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -12,26 +12,16 @@ const Highlights = () => {
                         Why choose <span className="purple">Library</span>?
                     </h2>
                     <div className="highlight__wrapper">
-                        <Highlight icon={<FontAwesomeIcon icon="bolt" />} title= "Easy and Quick"
-                        para = "Get access to the book you purchased online instantly. No waiting time." />
+                       <Highlight icon={<FontAwesomeIcon icon="bolt" />} title= "Easy and Quick"
+                        para= "Get access to the book you purchased online instantly. No waiting time." />
                         
-                        <div className="highlight">
-                            <div className="highlight__img">
-                                <FontAwesomeIcon icon="book-open" />
-                                </div>
-                            <h3 className="highlight__subtitle">10,000+ Books</h3>
-                            <p className="highlight__para">
-                                Library has books in all your favorite categories. </p>
-                        </div>
-                        <div className="highlight">
-                            <div className="highlight__img">
-                                <FontAwesomeIcon icon="tags" />
-                                </div>
-                            <h3 className="highlight__subtitle">Affordable</h3>
-                            <p className="highlight__para">
-                                Get your hands on popular titles for as little as $10.00
-                            </p>
-                        </div>
+                       <Highlight icon={<FontAwesomeIcon icon="book-open" />} title= "10,000+ Books"
+                        para= "Library has books in all your favorite categories." />
+                        
+                       <Highlight icon={<FontAwesomeIcon icon="tags" />} title= "Affordable"
+                        para= "Get your hands on popular titles for as little as $10.00" />
+                        
+                
                     </div>
                 </div>
             </div>
