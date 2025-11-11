e625da5ae44d78fb0724a52e2edb889d5dc00bda
Yiannis <ioannis.roussos@hotmail.com>
2025-11-11 18:46:49 +0200

    installed router

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import Featured from "./components/Featured";
 import Discounted from "./components/Discounted";
 import Explore from "./components/Explore";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
 function App() {
   return (
+    <Router>
+
     <div className="App">
      <Nav />
       <Landing />
@@ -17,6 +20,7 @@ function App() {
     <Explore />
     <Footer />
     </div>
+    </Router>
   );
 }
 
