97da97875b2f3b835b728d641bdf615ee5c79dab
Yiannis <ioannis.roussos@hotmail.com>
2025-11-13 23:43:11 +0200

    links not working

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 import Home from "./Pages/Home";
 import Books from "./Pages/Books";
 import {books} from './data'
+import BookInfo from "./Pages/BookInfo";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
      <Nav />
       <Route path= "/" exact component={Home} />
       <Route path= "/books" render ={() => <Books books={books} />}/>
+      <Route path="/books/1" render={() =><BookInfo books={books} />} />
            <Footer />
     </div>
   </Router>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import LibraryLogo from '../assets/Library.svg';
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 
 const Nav = () => {
@@ -15,26 +16,25 @@ function closeMenu() {
   return (
     <nav>
         <div className="nav__container">
-            <a href="/">
+            <Link to="/">
             <img src={LibraryLogo} alt="" className="logo"/>
-            </a>
-            <ul className="nav__links">
+            </Link>
+             <ul className="nav__links">
              <li className="nav__list">
-                <a href="/" className="nav__link">
-                Home</a>
-             </li>
+                <Link to="/" className="nav__link">
+                Home</Link>
+                </li>
              <li className="nav__list">
-                <a href="/" className="nav__link">
-                Books</a>
-             </li>
+                <Link to="/" className="nav__link">
+                Books</Link>          </li>
              <button className="btn__menu" onClick={openMenu}>
                 <FontAwesomeIcon icon="bars" />
              </button>
              <li className="nav__icon">
-                <a href="/cart" className="nav__link">
+                <Link to="/cart" className="nav__link">
                 <FontAwesomeIcon icon="shopping-cart" />
-                </a>
-                <span className="cart__length">3</span>
+                </Link>          
+                 <span className="cart__length">3</span>
              </li>
             </ul>
             <div className="menu__backdrop">
@@ -43,20 +43,19 @@ function closeMenu() {
                 </button>
                 <ul className="menu__links">
                     <li className="menu__list">
-                        <a href="/" className="menu__link">
+                        <Link to="/" className="menu__link">
                         Home
-                        </a>
-                    </li>
+                        </Link>
+                         </li>
                     <li className="menu__list">
-                        <a href="/books" className="menu__link">
-                        Books
-                        </a>
-                    </li>
+                        <Link to="/books" className="menu__link">
+                        Books</Link>
+                         </li>
                     <li className="menu__list">
-                        <a href="/cart" className="menu__link">
+                        <Link to="/cart" className="menu__link">
                         Cart
-                        </a>
-                    </li>
+                        </Link>
+                         </li>
                 </ul>
             </div>
         </div>
